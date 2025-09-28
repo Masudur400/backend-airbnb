a92ee8d3d02205e00915452e794597f2f143b98f
Masudur400 <md8004210@gmail.com>
2025-09-28 21:38:58 +0600

    basic file setup

---
+++ src/app/config/env.ts
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+
+dotenv.config()
+
+interface EnvConfig {
+    PORT: string,
+    DB_URL: string,
+    NODE_ENV: "development" | "production"
+    FRONTEND_URL1: string
+    FRONTEND_URL2: string
+    FRONTEND_URL3: string
+    BCRYPT_SALT_ROUND: string
+    JWT_ACCESS_SECRET: string
+    JWT_ACCESS_EXPIRES: string
+    JWT_REFRESH_SECRET: string
+    JWT_REFRESH_EXPIRES: string
+    EXPRESS_SESSION_SECRET: string
+}
+
+const loadEnvVariables = (): EnvConfig => {
+    const requiredEnvVariables: string[] = ["PORT",
+        "DB_URL",
+        "NODE_ENV",
+        "FRONTEND_URL1",
+        "FRONTEND_URL2",
+        "FRONTEND_URL3",
+        "BCRYPT_SALT_ROUND",
+        "JWT_ACCESS_SECRET",
+        "JWT_ACCESS_EXPIRES",
+        "JWT_REFRESH_SECRET",
+        "JWT_REFRESH_EXPIRES",
+        "EXPRESS_SESSION_SECRET"
+    ];
+
+    requiredEnvVariables.forEach(key => {
+        if (!process.env[key]) {
+            throw new Error(`Missing require environment variable ${key}`)
+        }
+    })
+
+    return {
+        PORT: process.env.PORT as string,
+        DB_URL: process.env.DB_URL!,
+        NODE_ENV: process.env.NODE_ENV as "development" | "production",
+        FRONTEND_URL1: process.env.FRONTEND_URL as string,
+        FRONTEND_URL2: process.env.FRONTEND_URL as string,
+        FRONTEND_URL3: process.env.FRONTEND_URL as string,
+        BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string ,
+        EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string
+    }
+}
+
+export const envVars = loadEnvVariables()
